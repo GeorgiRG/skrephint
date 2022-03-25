572c3947f0afa82210cde611678b9866f88bca19
Georgi <grgeorgi93@gmail.com>
2022-03-25 15:06:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace skrephint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
