439396e44ab19c7910a512a5ef15dd91d99b48ae
Georgi <grgeorgi93@gmail.com>
2022-04-03 00:02:14 +0300

    httpclient

--- Program.cs
+++ Program.cs
@@ -1,40 +1,65 @@
 ﻿using System;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
-using Npgsql;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace skrephint
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            var options = new FirefoxOptions();
+            string msg = await GetHtml();
+            Console.WriteLine(msg);
+        }
+
+        private static async Task<string> GetHtml()
+        {
             
-            using (var driver = new FirefoxDriver("pth", options))
-            {
-                // Go to the home page
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
+            client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br"); //needs decoding
+            client.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.5");
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0");
+            client.DefaultRequestHeaders.Add("Referer", "https://www.wikipedia.org/");
+            client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
+            
 
-                driver.Navigate().GoToUrl("https://www.wikipedia.org/");
-                //var userNameField = driver.FindElement(By.Id("usr"));
-                string output2 = driver.PageSource;
-                Console.WriteLine(output2);
-            }
-            /*
-            var cs = "Host=localhost;Username=postgres;Password=6165939597;Database=postgres";
+            var stringTask = client.GetStringAsync("https://en.wikipedia.org/wiki/Jack_Dongarra");
 
-            using var con = new NpgsqlConnection(cs);
-            con.Open();
-
-            var sql = "SELECT version()";
-
-            using var cmd = new NpgsqlCommand(sql, con);
-
-            Console.WriteLine("done till here");
-            var version = cmd.ExecuteScalar().ToString();
-            Console.WriteLine($"PostgreSQL version: {version}");
-            */
+            var msg = await stringTask;
+            //Console.Write(msg);
+            return msg;
         }
     }
 }
+/*
+var cs = "Host=localhost;Username=postgres;Password=6165939597;Database=postgres";
+
+using var con = new NpgsqlConnection(cs);
+con.Open();
+
+var sql = "SELECT version()";
+
+using var cmd = new NpgsqlCommand(sql, con);
+
+Console.WriteLine("done till here");
+var version = cmd.ExecuteScalar().ToString();
+Console.WriteLine($"PostgreSQL version: {version}");
+
+ //Console.WriteLine("Hello World!");
+var options = new FirefoxOptions();
+
+using (var driver = new FirefoxDriver("pth", options))
+{
+    // Go to the home page
+
+    driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+    //var userNameField = driver.FindElement(By.Id("usr"));
+    string output2 = driver.PageSource;
+    Console.WriteLine(output2);
+}
+*/
