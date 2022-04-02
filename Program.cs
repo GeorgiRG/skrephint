f7a598b726812666f76c6518c69af938d17fe9a8
Georgi <grgeorgi93@gmail.com>
2022-04-02 17:16:44 +0300

    test files, upgrading project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using Npgsql;
 
 namespace skrephint
 {
@@ -6,7 +9,32 @@ namespace skrephint
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            var options = new FirefoxOptions();
+            
+            using (var driver = new FirefoxDriver("C:\\Users\\elisa\\source\\repos\\skrephint", options))
+            {
+                // Go to the home page
+
+                driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+                //var userNameField = driver.FindElement(By.Id("usr"));
+                string output2 = driver.PageSource;
+                Console.WriteLine(output2);
+            }
+            /*
+            var cs = "Host=localhost;Username=postgres;Password=6165939597;Database=postgres";
+
+            using var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            var sql = "SELECT version()";
+
+            using var cmd = new NpgsqlCommand(sql, con);
+
+            Console.WriteLine("done till here");
+            var version = cmd.ExecuteScalar().ToString();
+            Console.WriteLine($"PostgreSQL version: {version}");
+            */
         }
     }
 }
